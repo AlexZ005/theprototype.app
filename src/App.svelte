43b59550c4d3c400ffc6cd83ffd9eb733acc760b
AlexZ005 <alexz005.info@gmail.com>
2024-10-28 21:02:52 +0200

    removing showGrid runes

--- src/App.svelte
+++ src/App.svelte
@@ -2,24 +2,10 @@
   import { Canvas } from '@threlte/core'
   import Scene from './components/Scene.svelte'
   import Menu from './components/Menu.svelte'
-  const toggleGridVisibility = () => {
-    toggleGrid = !toggleGrid;
-  };
-
-  let toggleGrid = $state(false);
-
-  $effect(() => {
-    console.log("showGrid2: ", toggleGrid)
-
-  })
 </script>
-
-<button on:click={toggleGridVisibility}>
-  Toggle Grid Visibility
-</button>
 
 <Menu />
 
 <Canvas>
-  <Scene showGrid={toggleGrid} />
+  <Scene />
 </Canvas>
