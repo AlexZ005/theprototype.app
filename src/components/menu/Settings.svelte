3a8067d7243e531fae62c4d5090edb04ccb8fcd0
AlexZ005 <alexz005.info@gmail.com>
2024-10-28 20:42:27 +0200

    [ui] options cover for parameters

--- src/components/menu/Settings.svelte
+++ src/components/menu/Settings.svelte
@@ -3,7 +3,8 @@
 		Accordion,
 		AccordionItem,
 		Modal,
-        Button
+        Button,
+        Checkbox
 	} from 'flowbite-svelte';
 
 	let clickOutsideModal = true;
@@ -15,6 +16,22 @@
 	<Accordion>
 		<AccordionItem>
 			<svelte:fragment slot="header">Scene</svelte:fragment>
+            <div class="flex ">
+                <p class="w-40 flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-tl-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">
+                  <Checkbox>&nbsp;VR override</Checkbox>
+                </p>
+                <p class="w-full z-10 inline-flex items-center py-2.5 px-5 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-tr-lg dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                  Forces normal play even if immersive-vr is enabled
+                </p>
+              </div>
+            <div class="flex ">
+              <p class="w-40 flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-bl-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">
+                <Checkbox>&nbsp;Show grid</Checkbox>
+              </p>
+              <p class="w-full z-10 inline-flex items-center py-2.5 px-5 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-br-lg dark:bg-gray-700 dark:text-white dark:border-gray-600">
+                Display grid on floor
+              </p>
+            </div>
 		</AccordionItem>
 		<AccordionItem>
 			<svelte:fragment slot="header">About</svelte:fragment>
