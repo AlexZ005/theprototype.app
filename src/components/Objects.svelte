12a71b14609866a699942386353a245f1b954fcb
AlexZ005 <alexz005.info@gmail.com>
2024-10-28 09:57:28 +0200

    addBox and clear scene from Objects.svelte using stores

---
+++ src/components/Objects.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as THREE from 'three';
+
+	import { globalScene } from '../stores/sceneStore.js';
+	// $: console.log($globalScene)
+
+	export function clear() {
+		// console.log($globalScene);
+		$globalScene.clear()
+		console.log('Clear objects');
+	}
+
+    export function addCube() {
+        let mesh = new THREE.BoxGeometry(2, 2, 2);
+		let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		let cube = new THREE.Mesh(mesh, material);
+		$globalScene.add(cube);
+    }
+
+</script>
