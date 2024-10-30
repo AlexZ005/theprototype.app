20d22e1049f5412a5b503f2cb54cd9222db00e57
AlexZ005 <alexz005.info@gmail.com>
2024-10-30 19:00:11 +0200

    commands for grid and cube

--- src/lib/commandsHandler.svelte.js
+++ src/lib/commandsHandler.svelte.js
@@ -1,4 +1,5 @@
-import { globalScene, objectsGroup } from '../stores/sceneStore.js';
+import * as THREE from 'three';
+import { globalScene, objectsGroup, showGrid } from '../stores/sceneStore.js';
 
 //Access scene Store
 let scene = $state();
@@ -9,7 +10,33 @@ let sceneObjects = $state();
 objectsGroup.subscribe(value => { sceneObjects = value });
 
 export function sceneCommand(command) {
-    console.log('Executing command: ' + command);
-    console.log(scene)
-    scene.clear()
+    if (command.startsWith('/')) {
+        console.log('Executing command: ' + command);
+        if (command.startsWith('/clear')) {
+            if (command.split(' ')[1] == 'all')
+                sceneObjects.clear();
+        }
+        else if (command.startsWith('/grid')) {
+            if (command.split(' ')[1] == 'on')
+                showGrid.set(true);
+            else if (command.split(' ')[1] == 'off')
+                showGrid.set(false);
+        }
+        else if (command.startsWith('/create')) {
+            if (command.split(' ')[1] == 'cube') {
+                if (command.split(' ')[2] && command.split(' ')[3] && command.split(' ')[4]) {
+                    createCube(command.split(' ')[2], command.split(' ')[3], command.split(' ')[4]);
+                } else {
+                    createCube();
+                }   
+            }
+        }
+    }
+}
+
+function createCube(x,y,z) {
+    let mesh = new THREE.BoxGeometry(x, y, z);
+    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    let cube = new THREE.Mesh(mesh, material);
+    sceneObjects.add(cube);
 }
